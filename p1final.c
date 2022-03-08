be6dc30bccc633344e33bf6b88bb2866c66e789f
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-03-08 03:32:49 +0000

    p2

--- p1final.c
+++ p1final.c
@@ -1,1 +1,34 @@
 #include <stdio.h>
+
+void input(int *num1, int *den1,int *num2,int *den2)
+{
+  printf("Enter a numerator and denominator:\n");
+  scanf("%d%d",num1,den1);
+  printf("Enter another numerator and denominator:\n");
+  scanf("%d%d",num2,den2); 
+}
+void add(int num1,int den1,int num2, int den2,int *num3,int *den3)
+{
+  int gcd=0;
+  int x=(num1*den2)+(num2*den1);
+  int y=den1*den2;
+  for(int i=1;i<=x && i<=y;++i)
+  {
+    if(x%i==0 && y%i==0)
+    gcd=i;
+  }
+  *den3=y/gcd;
+  *num3=x/gcd;
+}
+void output(int num1,int den1,int num2,int den2,int num3,int den3)
+{
+  printf("%d/%d + %de/%d is %d/%d\n",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,den1,num2,den2,num3,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
+}
--- p2final.c
+++ p2final.c
@@ -1,1 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
+
+typedef struct fraction
+{
+  int num,den;
+}fraction;
+fraction input_fraction()
+{
+  fraction a;
+  printf("Enter a fraction:\n");
+  scanf("%d%d",&a.num,&a.den);
+  return a;
+}
+fraction largest_fraction(fraction a,fraction b,fraction c)
+{
+  fraction large;
+  large.num=a.num;
+  large.den=a.den;
+  float f1=a.num/a.den;
+  float f2=b.num/b.den;
+  float f3=c.num/c.den;
+  if(f1>f2)
+    large.num=b.num;
+    large.den=b.den;
+  if(f1>f3)
+    large.num=c.num;
+    large.den=c.den;
+  return large;
+}
+void output(fraction a,fraction b, fraction c,fraction largest)
+{
+  printf("%d/%d is the smallest among %d/%d, %d/%d and %d/%d.\n",largest.num,largest.den,a.num,a.den,b.num,b.den,c.num,c.den);
+}
+int main()
+{
+  fraction a,b,c,small;
+  a=input_fraction();
+  b=input_fraction();
+  c=input_fraction();
+  small=largest_fraction(a,b,c);
+  output(a,b,c,small);
+  return 0;
+}
